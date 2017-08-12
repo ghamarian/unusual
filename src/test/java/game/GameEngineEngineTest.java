5cc037bea9678108876ff2fd115839351104c598
Amir Ghamarian <amir.ghamarian@pega.com>
2017-08-12 17:31:00 +0200

    First integration test.

--- src/test/java/game/GameEngineEngineTest.java
+++ src/test/java/game/GameEngineEngineTest.java
@@ -3,19 +3,27 @@ package game;
 import gameInterface.TextConsole;
 import org.junit.Before;
 import org.junit.Test;
-import strategy.RandomGuesser;
+import org.mockito.Mock;
 
 import java.util.Scanner;
 
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
 public class GameEngineEngineTest {
+
+    @Mock
+    Guesser guesser;
 
     @Before
     public void setUp() throws Exception {
-        GameEngine game = new GameEngine(new TextConsole(new Scanner(System.in)), new RandomGuesser());
+        initMocks(this);
     }
 
     @Test
     public void play() throws Exception {
-
+        when(guesser.nextGuess()).thenReturn(Shape.PAPER).thenReturn(Shape.PAPER).thenReturn(Shape.PAPER);
+        GameEngine game = new GameEngine(new TextConsole(new Scanner("3\nrock\nrock\nrock\n")), guesser);
+        game.play();
     }
 }
